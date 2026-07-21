4e8838fbbfab14854a616d3b6340cdfbba9a7250
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-21 22:58:14 +0530

    Implement user input for calculator operations
    Added user input options for calculator operations.

--- calculator.py
+++ calculator.py
@@ -20,3 +20,15 @@ def divide(x, y):
 
    return x / y
 
+# take input from the user
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice = input("Enter choice(1/2/3/4):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
