a762d68dd17ecd1dfece9f4cc09a0b6116b17c08
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-21 22:57:52 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -10,4 +10,13 @@ def subtract(x, y):
    """This function subtracts two numbers"""
 
    return x - y
+def multiply(x, y):
+   """This function multiplies two numbers"""
+
+   return x * y
+
+def divide(x, y):
+   """This function divides two numbers"""
+
+   return x / y
 
