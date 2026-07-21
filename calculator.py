c5b3e9da5d807445ba80511a7da3a8b03240731e
Ankur Ghosh <ankurghosh271005@gmail.com>
2026-07-21 22:56:36 +0530

    Implement addition function in calculator
    Added a function to perform addition of two numbers.

--- calculator.py
+++ calculator.py
@@ -1,1 +1,9 @@
+# Program make a simple calculator that can add, subtract, multiply and divide using functions
+
+# define functions
+def add(x, y):
+   """This function adds two numbers"""
+   
+
+   return x + y
 
